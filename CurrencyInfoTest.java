749f7f47bccf4748dfaec77edebe9a17a955273a
YanpengQi <32005883+YanpengQi@users.noreply.github.com>
2019-04-28 17:16:05 -0400

    add test cases

---
+++ CurrencyInfoTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CurrencyInfoTest {
+
+	@Test
+	public void testCurrencyInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveString() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveStringDouble() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSize() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ FindCycleProfitTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FindCycleProfitTest {
+
+	@Test
+	public void testFindCycleProfit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindCycle() {
+		fail("Not yet implemented");
+	}
+
+}
